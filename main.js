faae50c0b5352b8e250e4ce0e376bfb25f7217e8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-04-30 20:51:25 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// Vue.filter('number_format', function(val) {
+//   return val.toLocaleString();
+// });
+
+var app = new Vue ({
+
+  el: "#app",
+  data: {
+    show: true,
+    year: (new Date()).getFullYear(),
+    price: 1280,
+    stock: 10,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    products: [
+      {code: 'A01', name: 'product A'},
+      {code: 'B01', name: 'product B'},
+      {code: 'C01', name: 'product C'},
+    ]
+  },
+  created: function() {
+    addEventListener('resize', this.resizeHandler)
+  },
+  beforeDestroy: function() {
+    removeEventListener('resize', this.resizeHandler)
+  },
+  methods: {
+    now1: function(){
+      return (new Date()).toLocaleString();
+    },
+    // 削除ボタンのクリックイベントハンドラ
+    onDeleteItem: function () {
+      this.stock--;
+    },
+    resizeHandler: function($event) {
+      this.width = $event.target.innerWidth;
+      this.height = $event.target.innerHeight;
+    }
+  },
+  computed: {
+    isUrudoshi: function() {
+      if ((this.year%4 == 0) && (this.year%100 !=0 ) ||
+      (this.year%400 == 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    now2: function() {
+      return (new Date()).toLocaleString();
+    }
+  },
+
+  // filters: {
+  //   number_format: function(val) {
+  //     return val.toLocaleString();
+  //   },
+  //   unit: function(val) {
+  //     return val + '円';
+  //   }
+  // }
+});
