f4fcfbbf87e0061c19e11bf16d5462be40422ebf
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-03 13:51:49 +0900

    3章終了

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ var app = new Vue ({
     sortOrder: 1,
     // 商品リスト
     products: [
-      {name: 'Michael<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true},
-      {name: 'Raphael<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true},
-      {name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true},
-      {name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true},
-      {name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false},
-      {name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false},
+      {id:1, name: 'Michael<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true},
+      {id:2, name: 'Raphael<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true},
+      {id:3, name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true},
+      {id:4, name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true},
+      {id:5, name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false},
+      {id:6, name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false},
     ]
   },
 
@@ -37,7 +37,43 @@ var app = new Vue ({
       // ここでproductsの配列を書き換える
       console.log('showDelvFreeウォッチャが呼び出されました')
     },
-  }
+  },
+
+  computed: {
+    // 絞り込み後の商品リストを返す算出プロパティ
+    filteredList: function() {
+      // 絞り込み後の商品リストを格納する新しい配列
+      var newList = [];
+      for (var i=0; i<this.products.length; i++) {
+        // 表示対象かどうかを判定するフラグ
+        var isShow = true;
+        // i番目の商品が表示対象かどうかを判定する
+        if (this.showSaleItem && !this.products[i].isSale ) {
+          // 「セール対象」チェック有りで、セール対象商品でない場合
+          isShow = false;  // この商品は表示しない
+        }
+        // 「送料無料」チェック有りで、送料有りの商品の場合
+        if (this.showDelvFree && this.products[i].delv > 0) {
+          // その商品は表示しない
+          isShow = false;
+        }
+        // 表示対象の商品だけを新しい配列に追加する
+        if (isShow) {
+          newList.push(this.products[i]);
+        }
+      }
+      // 新しい配列を並び替える
+      if (this.sortOrder == 1) {
+        // 元の順番にpushしているので並び替え済み
+      } else if (this.sortOrder == 2) {
+        // 価格が安い順に並び替える
+        newList.sort(function(a,b) {
+          return a.price - b.price;
+        });
+      }
+      return newList;
+    },
+  },
 });
 
 // // コンポーネントのルートノード
